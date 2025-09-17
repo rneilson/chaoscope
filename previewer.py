fd8a95610d8927cb810ea510fb7af2170c6d7fa3
Raymond Neilson <rneilson@interloper.ca>
2025-09-16 23:41:34 -0300

    Use PIL instead of openCV for text overlay

--- previewer.py
+++ previewer.py
@@ -1,22 +1,28 @@
-import cv2  # pyright: ignore[reportMissingImports]
-import numpy as np
 import time
 
+import numpy as np
+from PIL import Image, ImageDraw, ImageFont
 from picamera2 import Picamera2, Preview  # pyright: ignore[reportMissingImports]
+
+FONT_PATH = '/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf'
 
 picam2 = Picamera2()
 picam2.configure(picam2.create_preview_configuration())
 picam2.start_preview(Preview.QTGL, width=640, height=480, x=0, y=0)
 picam2.start()
 
+font = ImageFont.truetype(FONT_PATH, 24)
+text_color = (239, 239, 239, 255)
+text_origin = (10, 10)
+
 for time_left in range(10, 0, -1):
-    colour = (0, 255, 0, 255)
-    origin = (0, 30)
-    font = cv2.FONT_HERSHEY_DUPLEX
-    scale = 1
-    thickness = 2
-    overlay = np.zeros((640, 480, 4), dtype=np.uint8)
-    cv2.putText(overlay, f"Remaining: {time_left:2}s", origin, font, scale, colour, thickness)
+    text_overlay = Image.new('RGBA', (640, 480), (255, 255, 255, 0))
+    draw = ImageDraw.Draw(text_overlay)
+    text = f"Remaining: {time_left:2}s"
+
+    draw.text(text_origin, text, font=font, fill=text_color)
+
+    overlay = np.asarray(text_overlay)
     picam2.set_overlay(overlay)
     time.sleep(1)
 
