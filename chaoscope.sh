7d598c1d698f6d5a13de11156103045cf5ea5b0d
Raymond Neilson <rneilson@interloper.ca>
2025-09-29 12:37:09 -0300

    Attempt to have wrapper script forward SIGINT/SIGTERM

--- chaoscope.sh
+++ chaoscope.sh
@@ -1,13 +1,43 @@
 #!/bin/bash
 
 do_shutdown=0
+child_pid=""
+
+handle_term() {
+    if [[ -n "$child_pid" ]]; then
+        echo "Interrupting child PID $child_pid"
+        kill -INT "$child_pid" 2>/dev/null
+    else
+        echo "No child PID to interrupt"
+    fi
+}
+
+setup_term() {
+    child_pid=""
+    # TODO: harder kill for SIGTERM?
+    trap 'handle_term' TERM INT
+}
+
+echo "Starting application..."
 
 while true; do
-    # TODO: parameterize
-    /usr/bin/python3 ~/chaoscope/chaoscope.py
-    if [[ $? -eq 2 ]]; then
-        do_shutdown=1
-        break
+    setup_term
+    # TODO: parameterize dir
+    /usr/bin/python3 ~/chaoscope/chaoscope.py &
+    child_pid=$!
+    echo "Child PID: ${child_pid:-none}"
+
+    if [[ -n "$child_pid" ]]; then
+        # Wait once, will return after trap kill
+        wait "$child_pid" 2>/dev/null
+        # Clear trap and wait again to get actual exit code
+        trap - TERM INT
+        wait "$child_pid" 2>/dev/null
+
+        if [[ $? -eq 2 ]]; then
+            do_shutdown=1
+            break
+        fi
     fi
     echo "Restarting application..."
 done
