6a06571a98d1ca28916d5af638eeb48ccd66ace8
Raymond Neilson <rneilson@interloper.ca>
2025-10-16 23:52:18 -0300

    It seems as though just averaging the last half-second's worth of attitude quaternions works better?

--- accel.py
+++ accel.py
@@ -1,14 +1,14 @@
 import json
 import sys
+from collections import deque
 from pathlib import Path
 from time import sleep
+from typing import Sequence
 
+import numpy as np
 from ahrs import DEG2RAD, RAD2DEG
-
-# from ahrs.common.orientation import ecompass
 from ahrs.common.quaternion import Quaternion
 from ahrs.filters.tilt import Tilt
-from numpy import array
 from smbus2 import SMBus
 
 from chaoscope_lib.inertial import IMU, ONE_G
@@ -31,6 +31,19 @@ def show_cursor() -> None:
     write_stdout("\033[?25h")
 
 
+def avg_quats(quats: Sequence[Quaternion]) -> Quaternion:
+    accum = np.array([0.0, 0.0, 0.0, 0.0])
+    # TODO: setup weights to favor most recent
+
+    for q in quats:
+        if np.dot(accum, q) < 0.0:
+            accum -= q
+        else:
+            accum += q
+
+    return Quaternion(accum)
+
+
 def test_accel(
     i2c_bus_num: int,
     mag_calibration: MagnometerCalibration,
@@ -49,30 +62,36 @@ def test_accel(
     print()
 
     # For rotating to NED frame
-    qr = Quaternion(rpy=array([0.0, 0.0, 90.0]) * DEG2RAD)
+    qr = Quaternion(rpy=np.array([0.0, 0.0, 90.0]) * DEG2RAD)
+
+    # Quaternions to do a running average of
+    qq = deque(maxlen=20)  # Half second at 40 Hz
 
     while True:
         acc_out = imu_obj.get_scaled_accel()
         mag_out = mag_obj.get_scaled_mag()
 
-        acc = array([v * ONE_G for v in acc_out])  # m/s^2
-        mag = array(mag_out)  # uT
+        acc = np.array([v * ONE_G for v in acc_out])  # m/s^2
+        mag = np.array(mag_out)  # uT
 
         heading = Quaternion(
             Tilt().estimate(acc=acc * -1, mag=mag, representation="quaternion")
         )
         heading_rot = Quaternion(heading * qr)
+
+        qq.append(heading_rot)
+        heading_avg = avg_quats(qq)
+
         ax, ay, az = acc_out
         mx, my, mz = mag_out
-        roll, pitch, yaw = (float(v) for v in (heading_rot.to_angles() * RAD2DEG))
-
+        roll, pitch, yaw = (float(v) for v in (heading_avg.to_angles() * RAD2DEG))
         write_stdout(
             f"\rax: {ax: 6.3f} ay: {ay: 6.3f} az: {az: 6.3f} "
-            f"mx: {mx: 6.1f} my: {my: 6.1f} mz: {mz: 6.1f} "
+            f"mx: {mx: 7.1f} my: {my: 7.1f} mz: {mz: 7.1f} "
             f"R: {roll: 4.0f}° P: {pitch: 4.0f}° Y: {yaw: 4.0f}°"
         )
 
-        sleep(0.1)
+        sleep(0.025)  # 40 Hz
 
 
 if __name__ == "__main__":
